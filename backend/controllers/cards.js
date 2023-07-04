1d5fc1eebcf908b918c2419b506d60a50c345c3d
Evgeniy Dekhtyarev <f-k-87@list.ru>
2023-07-04 14:54:32 +0300

    test

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -20,7 +20,6 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user;
   Card.create({ name, link, owner })
-    .then((card) => card.populate(['owner', 'name', 'avatar']))
     .then((card) => res.status(201).send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
